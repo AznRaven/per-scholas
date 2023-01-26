6f8b8a82cfe9bf7cfcbdddb14d081495af370ff1
AznRaven <versatilias@gmail.com>
2023-01-26 13:59:46 -0500

    create aliens and player done

--- Project/Space Battle/SpaceBattle.js
+++ Project/Space Battle/SpaceBattle.js
@@ -3,28 +3,50 @@
 // make a class ship
 class Ship {
     constructor(hull, firePower, accuracy) {
-        this.hull = hull || 20
-        this.firePower = firePower || 5
-        this.accuracy = accuracy || .7
+      this.hull = hull || 20
+      this.firePower = firePower || 5
+      this.accuracy = accuracy || .7
     }
-    attack() {}
-}
-
-const USSAssembly = new Ship()
-console.log(USSAssembly)
-
-let alienHull = Math.floor(Math.random(3,6)*6)
-let alienFirePower = Math.random(2,4)
-let alienAccuracy = (Math.random(.6,.8)*.9).toFixed(1)
-
-const alien1 = new Ship(alienHull, alienFirePower, alienAccuracy)
-const alien2 = new Ship(alienHull, alienFirePower, alienAccuracy)
-const alien3 = new Ship(alienHull, alienFirePower, alienAccuracy)
-const alien4 = new Ship(alienHull, alienFirePower, alienAccuracy)
-const alien6 = new Ship(alienHull, alienFirePower, alienAccuracy)
-console.log(alien1)
-
-// make a ship factory will pop out 6 aliens and your ship
-// set each alien properties using random
-// set your own ship properties
-// play the game
+    attack() { }
+  }
+  // alien ship factory
+  class Factory {
+    constructor(alien) {
+      this.alien = alien
+      this.alienShip = []
+    }
+    generateShip(numShips) {
+      let counter = 1
+      
+      while (counter <= numShips) {
+        let hull = Math.floor(Math.random() * (4) + 3)
+        let firePower = Math.floor(Math.random() * (3) + 2)
+        let accuracy = Number(((Math.random() * (2) + 6) / 10).toFixed(1))
+  
+        const newShip = new Ship(hull, firePower, accuracy)
+        this.alienShip.push(newShip)
+        counter++
+      }
+  
+    }
+    findShip(index) {
+      return this.alienShip[index]
+    }
+  }
+  
+  // new alien factory
+  const alien = new Factory('Alien')
+  
+  // generate 6 aliens
+  alien.generateShip(6)
+  console.log(alien)
+  console.log(alien.alienShip[3])
+  
+  // create player
+  const USSAssembly = new Ship()
+  console.log(USSAssembly)
+  
+  console.log("There are 6 alien ships!")
+  
+  // play the game
+  
