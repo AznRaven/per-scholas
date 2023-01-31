26daa631e0aa8ccaa35cfbec91fa22a6b8fe00cf
AznRaven <versatilias@gmail.com>
2023-01-31 17:54:43 -0500

    trivia

---
+++ Project/html_game/app.js
@@ -0,0 +1,518 @@
+questions = [
+    {
+        "question": "What does CSS stand for?",
+        "choices": [
+            "Compact Style Sheets",
+            "Cascading Style Sheets",
+            "Creative Style Sheets",
+            "Centralized Style Sheets"
+        ],
+        "answer": "B"
+    },
+    {
+        "question": "How do you set the background color of a webpage in CSS?",
+        "choices": [
+            "background: color;",
+            "bg-color: color;",
+            "bgcolor: color;",
+            "background-color: color;"
+        ],
+        "answer": "D"
+    },
+    {
+        "question": "How do you select all elements of a specific type in CSS?",
+        "choices": [
+            "type",
+            "*type",
+            ".type",
+            "type"
+        ],
+        "answer": "A"
+    },
+    {
+        "question": "How do you set the font family in CSS?",
+        "choices": [
+            "font: family;",
+            "font-family: family;",
+            "text-font: family;",
+            "f-family: family;"
+        ],
+        "answer": "B"
+    },
+    {
+        "question": "How do you make text bold in CSS?",
+        "choices": [
+            "text-bold: true;",
+            "bold: true;",
+            "font-weight: bold;",
+            "font-style: bold;"
+        ],
+        "answer": "C"
+    },
+    {
+        "question": "How do you set the width of an element in CSS?",
+        "choices": [
+            "width: value;",
+            "size: value;",
+            "w: value;",
+            "width-size: value;"
+        ],
+        "answer": "A"
+    },
+    {
+        "question": "How do you set the text color in CSS?",
+        "choices": [
+            "color: value;",
+            "text-color: value;",
+            "f-color: value;",
+            "font-color: value;"
+        ],
+        "answer": "A"
+    },
+    {
+        "question": "How do you specify multiple CSS styles for a single element?",
+        "choices": [
+            "Separate each style with a comma",
+            "Separate each style with a semicolon",
+            "Separate each style with a colon",
+            "List each style in its own class"
+        ],
+        "answer": "B"
+    },
+    {
+        "question": "How do you center an element horizontally in CSS?",
+        "choices": [
+            "center: true;",
+            "margin: 0 auto;",
+            "text-align: center;",
+            "align: center;"
+        ],
+        "answer": "B"
+    },
+    {
+        "question": "How do you set the font size in CSS?",
+        "choices": [
+            "font-size: value;",
+            "text-size: value;",
+            "size: value;",
+            "f-size: value;"
+        ],
+        "answer": "A"
+    },
+    {
+        "question": "What is the CSS property used to change the background color of an element?",
+        "choices": [
+            "background",
+            "background-color",
+            "bg-color",
+            "color"
+        ],
+        "answer": "B"
+    },
+    {
+        "question": "What is the CSS property used to control the space between elements?",
+        "choices": [
+            "margin",
+            "padding",
+            "spacing",
+            "gap"
+        ],
+        "answer": "A"
+    },
+    {
+        "question": "What is the CSS property used to set the border of an element?",
+        "choices": [
+            "border",
+            "edge",
+            "frame",
+            "outline"
+        ],
+        "answer": "A"
+    },
+    {
+        "question": "What is the CSS property used to set the font size of text?",
+        "choices": [
+            "font-size",
+            "font",
+            "text-size",
+            "size"
+        ],
+        "answer": "A"
+    },
+    {
+        "question": "What is the CSS selector used to select an element based on its id?",
+        "choices": [
+            ".id",
+            "#id",
+            "id",
+            "@id"
+        ],
+        "answer": "B"
+    },
+    {
+        "question": "What is the default value for the CSS display property?",
+        "choices": [
+            "block",
+            "inline",
+            "inline-block",
+            "none"
+        ],
+        "answer": "B"
+    },
+    {
+        "question": "What is the purpose of CSS?",
+        "choices": [
+            "To control the layout and design of web pages",
+            "To add content to web pages",
+            "To program interactive elements on web pages",
+            "To store data on web pages"
+        ],
+        "answer": "A"
+    },
+    {
+        "question": "What is the syntax for defining a CSS class?",
+        "choices": [
+            ".class { }",
+            "#class { }",
+            "class { }",
+            "class:"
+        ],
+        "answer": "A"
+    },
+    {
+        "question": "What does 'JS' stand for in JavaScript?",
+        "choices": [
+            "Java Standard",
+            "JavaScript",
+            "Java Syntax",
+            "Java Server"
+        ],
+        "answer": "B"
+    },
+    {
+        "question": "What is the syntax for declaring a variable in JavaScript?",
+        "choices": [
+            "var name = value",
+            "let name = value",
+            "const name = value",
+            "define name = value"
+        ],
+        "answer": "A"
+    },
+    {
+        "question": "Which of the following is NOT a JavaScript data type?",
+        "choices": [
+            "String",
+            "Number",
+            "Boolean",
+            "Object"
+        ],
+        "answer": "D"
+    },
+    {
+        "question": "What is the symbol used for concatenating strings in JavaScript?",
+        "choices": [
+            "+",
+            "&",
+            "%",
+            "*"
+        ],
+        "answer": "A"
+    },
+    {
+        "question": "What does 'DOM' stand for in web development?",
+        "choices": [
+            "Document Object Model",
+            "Direct Object Method",
+            "Direct Object Model",
+            "Document Operating Method"
+        ],
+        "answer": "A"
+    },
+    {
+        "question": "What is the symbol used for accessing an object's properties in JavaScript?",
+        "choices": [
+            "$",
+            ". (dot)",
+            "()",
+            "[]"
+        ],
+        "answer": "B"
+    },
+    {
+        "question": "What is the default value of a variable declared in JavaScript?",
+        "choices": [
+            "0",
+            " (empty string)",
+            "null",
+            "undefined"
+        ],
+        "answer": "D"
+    },
+    {
+        "question": "What does 'HTML' stand for in web development?",
+        "choices": [
+            "Hyper Text Markup Language",
+            "Hyperlinks and Text Markup Language",
+            "Hyper Text Making Language",
+            "Hyper Text Module Language"
+        ],
+        "answer": "A"
+    },
+    {
+        "question": "What is the tag used for creating a hyperlink in HTML?",
+        "choices": [
+            "&lt;a&gt;",
+            "&lt;link&gt;",
+            "&lt;hyperlink&gt;",
+            "&lt;url&gt;"
+        ],
+        "answer": "A"
+    },
+    {
+        "question": "What is the tag used for creating a list in HTML?",
+        "choices": [
+            "&lt;ol&gt;",
+            "&lt;list&gt;",
+            "&lt;ul&gt;",
+            "&lt;li&gt;"
+        ],
+        "answer": "C"
+    },
+    {
+        "question": "What is the tag used for creating a header in HTML?",
+        "choices": [
+            "&lt;header&gt;",
+            "&lt;head&gt;",
+            "&lt;h&gt;",
+            "&lt;title&gt;",
+        ],
+        "answer": "B"
+    },
+    {
+        "question": "What is the tag used for creating a table in HTML?",
+        "choices": [
+            "&lt;table&gt;",
+            "&lt;tab&gt;",
+            "&lt;grid&gt;",
+            "&lt;matrix&gt;"
+        ],
+        "answer": "A"
+    },
+    {
+        "question": "What is the tag used for creating a form in HTML?",
+        "choices": [
+            "&lt;form&gt;",
+            "&lt;input&gt;",
+            "&lt;submit&gt;",
+            "&lt;button&gt;"
+        ],
+        "answer": "A"
+    },
+    {
+        "question": "What is the tag used for creating a text input in a form in HTML?",
+        "choices": [
+            "&lt;input type='text'&gt;",
+            "&lt;input type='input'&gt;",
+            "&lt;text&gt;",
+            "&lt;txt&gt;"
+        ],
+        "answer": "A"
+    }
+]
+
+var ansDiv = document.querySelector('.answerDiv')
+var log = document.querySelector('.log')
+var win = document.querySelector('.win')
+var pTurn = document.querySelector('.turn')
+var p1p = document.querySelector('.p1p')
+var p2p = document.querySelector('.p2p')
+var p1D = document.querySelector('.player1D')
+var p2D = document.querySelector('.player2D')
+// var x = document.querySelector('.question')
+var answer = ''
+var selectedAnswer = ''
+var l = document.querySelector('.l')
+var r = document.querySelector('.r')
+var counter = 0
+var index = 0
+var p1points = 0
+var p2points = 0
+var winSound = new sound("win.mp3")
+var rSound = new sound("right.mp3")
+var wSound = new sound("wrong.mp3")
+var bgSound = new sound("bg1.mp3")
+
+function sound(src) {
+    this.sound = document.createElement("audio");
+    this.sound.src = src;
+    this.sound.setAttribute("preload", "auto");
+    this.sound.setAttribute("controls", "none");
+    this.sound.style.display = "none";
+    document.body.appendChild(this.sound);
+    this.play = function () {
+        this.sound.play();
+    }
+    this.stop = function () {
+        this.sound.pause();
+    }
+}
+
+function startGame() {
+    // log.innerHTML = (`length: ${questions.length} p1: ${p1points} p2: ${p2points}`)
+    log.innerHTML = playerStart
+    if (questions.length == 1 && p1points > p2points) {
+        document.querySelector('.question').style.display = "none"
+        pTurn.innerHTML = 'Player 1 Wins!!!'
+        winSound.play()
+        process.exit()
+
+    } else if (questions.length == 1 && p2points > p1points) {
+        document.querySelector('.question').style.display = "none"
+        pTurn.innerHTML = 'Player 2 Wins!!!'
+        winSound.play()
+        process.exit()
+    }
+    if (playerStart == 1) {
+        pTurn.innerHTML = "Player 1's Turn "
+        p1D.style.backgroundColor = "Green"
+        p2D.style.backgroundColor = "Purple"
+    } else {
+        pTurn.innerHTML = "Player 2's Turn "
+        p2D.style.backgroundColor = "Green"
+        p1D.style.backgroundColor = "Purple"
+    }
+    pTurn.innerHTML += questions.length
+    // randomize index for question
+    index = Math.floor(Math.random() * questions.length)
+
+    // show question to html
+    document.querySelector('.ask').textContent = `"${questions[index].question}"`
+
+    win.innerHTML = questions[index].answer
+    answer = questions[index].answer
+    // clear answer
+    ansDiv.innerHTML = ''
+
+    //show choices to html
+    for (let i = 0; i < 4; i++) {
+        // log.innerHTML += &lt;br> ${questions[index].choices[i]}`
+        let addLetter = ''
+        i == 0 ? addLetter = 'A' :
+            i == 1 ? addLetter = 'B' :
+                i == 2 ? addLetter = 'C' :
+                    addLetter = 'D'
+
+        // console.log(ansDiv)
+        ansDiv.innerHTML += `<p class="answer ${addLetter}">${addLetter} - ${questions[index].choices[i]}</p>`
+    }
+
+    // ansDiv.addEventListener('click', selectChoice)
+
+    // function selectChoice(e) {
+
+    //     if (e.target.tagName == 'P') {
+    //         // console.log(e.target.className)
+    //         // console.log(e.target.classList.contains(answer))
+    //         l.innerHTML += '<br>'
+    //         r.innerHTML += '<br>'
+    // for (let i = 0; i < questions.length; i++) {
+    //     l.innerHTML += `<br> ${questions[i].question}`
+    // }
+    //         questions.splice(index, 1)
+    //         for (let i = 0; i < questions.length; i++) {
+    //             r.innerHTML += &lt;br> ${questions[i].question}`
+    //         }
+
+    //         // remove used question
+
+    //         // e.target.classList.contains(answer) ? startGame(playerStart) : startGame(!playerStart)
+    //     } else {
+    //         return
+    //     }
+
+
+
+    // }
+    // log.innerHTML = questions.length
+    // log.innerHTML += index
+    // for(let i = 0; i < questions.length; i++){
+    //     l.innerHTML += &lt;br> ${questions[i].question}`
+    // }
+    // // remove used question
+    // questions.splice(index, 1)
+    // for(let i = 0; i < questions.length; i++){
+    //     r.innerHTML += &lt;br> ${questions[i].question}`
+    // }
+    // log.innerHTML += questions.length
+
+
+    function selecteAnswer() {
+
+    }
+}
+
+ansDiv.addEventListener('click', selectChoice)
+
+function selectChoice(e) {
+
+    if (e.target.tagName == 'P') {
+        // console.log(e.target.className)
+        // console.log(e.target.classList.contains(answer))
+        // l.innerHTML += '<br>'
+        // r.innerHTML += '<br>'
+        // for (let i = 0; i < questions.length; i++) {
+        //     l.innerHTML += &lt;br> ${questions[i].question}`
+        // }
+
+        // remove used question
+        questions.splice(index, 1)
+
+        // for (let i = 0; i < questions.length; i++) {
+        //     r.innerHTML += &lt;br> ${questions[i].question}`
+        // }
+
+        if (e.target.classList.contains(answer)) {
+            playerStart = playerStart
+            e.target.style.backgroundColor = "green"
+            rSound.play()
+        } else {
+            playerStart == 1 ? playerStart = 2 : playerStart = 1
+            log.innerHTML = `2 ${playerStart}`
+            e.target.style.backgroundColor = "red"
+            ansDiv.children[0].classList.contains(answer) ? ansDiv.children[0].style.backgroundColor = "green" :
+                ansDiv.children[1].classList.contains(answer) ? ansDiv.children[1].style.backgroundColor = "green" :
+                    ansDiv.children[2].classList.contains(answer) ? ansDiv.children[2].style.backgroundColor = "green" :
+                        ansDiv.children[3].style.backgroundColor = "green"
+            wSound.play()
+        }
+        console.log(playerStart)
+        console.log(answer)
+
+        playerStart == 1 && e.target.classList.contains(answer) ? p1points += 1 :
+            playerStart == 2 && e.target.classList.contains(answer) ? p2points += 1 :
+                null
+
+        p1p.innerHTML = `Total Points: ${p1points}`
+        p2p.innerHTML = `Total Points: ${p2points}`
+        // e.target.classList.contains(answer) ? startGame(playerStart) : startGame(!playerStart)
+
+        // win.innerHTML = 'hi'
+        setTimeout(startGame, 2000)
+        // startGame(playerStart)
+    } else {
+        return
+    }
+}
+
+
+var playerStart = Math.floor(Math.random(1) * 2) + 1
+
+// coin == 1 ? playerStart = true : playerStart = false
+
+// bgSound.play()
+// bgSound.volume = 0.5
+var audio = document.getElementById("myAudio");
+audio.volume = 0.5; // set volume to 50%
+audio.play();
+startGame()
