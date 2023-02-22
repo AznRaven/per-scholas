7fc54dfc0051a80dc7d8780dd9a4c206ba38c938
AznRaven <versatilias@gmail.com>
2023-02-21 19:53:01 -0500

    stock hw

--- HW/stock/src/App.js
+++ HW/stock/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import About from "./components/About";
+import Stock from "./components/Stock";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={Home} />
+      <Route path="/dashboard" element={Dashboard} />
+      <Route path="/stock" element={Stock} />
+      <Route path="/about" element={About} />
+    </Routes>
   );
 }
 
